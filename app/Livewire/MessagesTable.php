eccd44b0739edb333c2975cf66421c9b1386ced0
Fabio Camargo <fabiorcamargo@gmail.com>
2024-04-04 17:04:11 -0300

    up

--- app/Livewire/MessagesTable.php
+++ app/Livewire/MessagesTable.php
@@ -13,16 +13,16 @@ class MessagesTable extends Component
     use WithPagination;
     public $wpp;
 
-    public function atualizarTabela()
-    {
-        // Lógica para atualizar a tabela aqui
-        $this->wpp->load('Messages');
-        // Outras lógicas de atualização, se necessário
-    }
+    // public function atualizarTabela()
+    // {
+    //     // Lógica para atualizar a tabela aqui
+    //     dd($this->wpp->load('Messages'));
+    //     // Outras lógicas de atualização, se necessário
+    // }
 
     public function render()
     {
-        $this->atualizarTabela();
+        // $this->atualizarTabela();
         
         return view('livewire.messages-table', [
             'mensagens' => $this->wpp->Messages()->orderBy('updated_at', 'desc')->paginate(10, pageName: 'message')
