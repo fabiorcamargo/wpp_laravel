92894247d5ac970545052bbcdd142f76df461cc4
Fabio Camargo <fabiorcamargo@gmail.com>
2025-01-31 19:03:58 -0300

    up

--- app/Console/Commands/RetryFailedJobsWithDelay.php
+++ app/Console/Commands/RetryFailedJobsWithDelay.php
@@ -4,7 +4,6 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\DB;
 use Laravel\Horizon\Contracts\JobRepository;
 
 class RetryFailedJobsWithDelay extends Command
@@ -13,21 +12,17 @@ class RetryFailedJobsWithDelay extends Command
     protected $description = 'Reprocessa jobs com falha no Horizon com um intervalo entre cada tentativa';
 
     public function handle(JobRepository $jobs)
-{
-    $failedJobs = $jobs->getFailed(); // Obtém os jobs com falha
+    {
+        $failedJobs = $jobs->getFailed(); // Pega os jobs com falha no Horizon
 
-    foreach ($failedJobs as $job) {
-        if (!DB::table('failed_jobs')->where('id', $job->id)->exists()) {
-            continue; // Pula jobs que já foram processados com sucesso
+        foreach ($failedJobs as $job) {
+            $jobId = $job->id;
+            $this->info("Reprocessando job ID: $jobId");
+            Artisan::call("queue:retry $jobId");
+            sleep($this->argument('delay')); // Aguarda X segundos antes do próximo
         }
 
-        $jobId = $job->id;
-        $this->info("Reprocessando job ID: $jobId");
-        Artisan::call("queue:retry $jobId");
-        sleep($this->argument('delay'));
+        $this->info("Todos os jobs com falha foram reprocessados!");
     }
-
-    $this->info("Todos os jobs pendentes foram reprocessados!");
-}
 }
 
