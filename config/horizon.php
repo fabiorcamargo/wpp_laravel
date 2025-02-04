7ddea3b94c4f74ce4733cbb504e19002c68b19d9
Fabio Camargo <fabiorcamargo@gmail.com>
2025-02-04 11:55:27 -0300

    up

--- config/horizon.php
+++ config/horizon.php
@@ -182,7 +182,7 @@ return [
     'defaults' => [
         'supervisor-1' => [
             'connection' => 'redis',
-            'queue' => ['zapfabio'],
+            'queue' => ['default', 'zapfabio'],
             'balance' => 'auto',
             'autoScalingStrategy' => 'time',
             'maxProcesses' => 5,
